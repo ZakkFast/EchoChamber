5e9191e1fb6cba0c1a864ff7529d6092768ae55b
Zakk Fast <zakkfastpro@gmail.com>
2021-09-23 21:07:34 -0700

    set up route for homepage

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const { Post, User, Comment } = require("../models");
+const sequelize = require("../config/connection");
+
+router.get("/", async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+          include: {
+            model: User,
+            attributes: ["user_name"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["user_name"]
+        }
+      ],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("homepage", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
