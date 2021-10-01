ecf738272af8d1924ae8ff04dc9bfe83a6224f29
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-09-30 23:11:28 -0700

    completed comment.js, added if loggedIn statment to view-single-post

---
+++ public/js/comment.js
@@ -0,0 +1,30 @@
+async function commentFromHandler(event) {
+    event.preventDefault();
+
+    const content= document.querySelector('textarea[name="content"]').nodeValue.trim();
+
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
+
+    if (content) {
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                content
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (response.ok) {
+            document.location.reload();
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+document.querySelector('.comment-body').addEventListener('submit', commentFromHandler);
