3cc19eb993797adc8083ad5ffcda59a2737dfa89
Zakk Fast <zakkfastpro@gmail.com>
2021-10-01 22:19:07 -0700

    fixed logout function.

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,6 +1,6 @@
 const logout = async () => {
     console.log('button clicked')
-    const response = await fetch('/api/user/logout', {
+    const response = await fetch('/api/users/logout', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
     });
